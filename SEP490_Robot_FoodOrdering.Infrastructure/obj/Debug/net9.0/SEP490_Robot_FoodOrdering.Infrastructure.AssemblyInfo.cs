c995dfb54f126e04464e7aef8efa37d49134fc65
nhattruowng <ltn04098@gmail.com>
2025-07-23 23:27:14 +0700

    addgitignore

--- SEP490_Robot_FoodOrdering.Infrastructure/obj/Debug/net9.0/SEP490_Robot_FoodOrdering.Infrastructure.AssemblyInfo.cs
+++ SEP490_Robot_FoodOrdering.Infrastructure/obj/Debug/net9.0/SEP490_Robot_FoodOrdering.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SEP490_Robot_FoodOrdering.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3cddfd4e30dab6b17646904bbe808cf6ae125c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e657655ae74e123e08be0f996508675a8ef79d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SEP490_Robot_FoodOrdering.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SEP490_Robot_FoodOrdering.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
