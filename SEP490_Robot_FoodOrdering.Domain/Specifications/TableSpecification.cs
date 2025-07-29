65e7f71c2b53ceec2b0744cd4f07ebcae2ce2788
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-07-29 07:37:53 +0700

    feature/table : fix get all table with condition of status

--- SEP490_Robot_FoodOrdering.Domain/Specifications/TableSpecification.cs
+++ SEP490_Robot_FoodOrdering.Domain/Specifications/TableSpecification.cs
@@ -13,14 +13,14 @@ namespace SEP490_Robot_FoodOrdering.Domain.Specifications
     {
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
-        public TableSpecification(int pageIndex, int pageSize)
-            : base(t => !t.DeletedTime.HasValue &&  t.Status==TableEnums.Available ) // No specific filter, just pagination
+        public TableSpecification(int pageIndex, int pageSize, TableEnums? status)
+    : base(t => !t.DeletedTime.HasValue && (!status.HasValue || t.Status == status.Value))
         {
             PageIndex = pageIndex;
             PageSize = pageSize;
-            // Include related entities if necessary
-            ApplyInclude(q => q
-                .Include(q => q.Orders))    ;
+
+            ApplyInclude(q => q.Include(q => q.Orders));
         }
+
     }
 }
