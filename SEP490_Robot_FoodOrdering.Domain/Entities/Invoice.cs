9bdeff17fd68e91a294e1e9fc3330502e10618f9
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-08-29 01:53:41 +0700

    Feature/fix_28082025 : update getInvoicebyTable

--- SEP490_Robot_FoodOrdering.Domain/Entities/Invoice.cs
+++ SEP490_Robot_FoodOrdering.Domain/Entities/Invoice.cs
@@ -5,7 +5,7 @@ namespace SEP490_Robot_FoodOrdering.Domain.Entities;
 
 public class Invoice: BaseEntity
 {
-    public Table Table { get; set; }
+    public virtual Table Table { get; set; }
     public decimal totalMoney { get; set; }        
     public PaymentStatusEnums status { get; set; }        
     public PaymentMethodEnums PhuongThucThanhToan { get; set; }
@@ -16,7 +16,8 @@ public class Invoice: BaseEntity
 public class InvoiceDetail : BaseEntity
 {
     // neu ma khach chon thi set OrderStatus thanh da huy va totol monney = 0 OK 
-    public OrderItem OrderItem { get; set; }
+   
+    public virtual OrderItem OrderItem { get; set; }
     public decimal totalMoney { get; set; }
     public OrderStatus Status { get; set; }
     public Invoice Invoice { get; set; }
---
+++ SEP490_Robot_FoodOrdering.Domain/Specifications/InvoiceSpecification.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SEP490_Robot_FoodOrdering.Domain.Entities;
+
+namespace SEP490_Robot_FoodOrdering.Domain.Specifications
+{
+    public class InvoiceSpecification : BaseSpecification<Invoice>
+    {
+        // Constructor cho paging
+        //public InvoiceSpecification(int pageIndex, int pageSize)
+        //    : base(pageIndex, pageSize)
+        //{
+        //    AddInvoiceIncludes();
+        //}
+
+        // Constructor cho Id
+        public InvoiceSpecification(Guid invoiceId)
+            : base(x => x.Id == invoiceId)
+        {
+            AddInvoiceIncludes();
+        }
+
+        // Constructor cho TableId + paging
+        public InvoiceSpecification(Guid tableId, int pageIndex, int pageSize)
+                : base(x => x.Table.Id == tableId)
+        {
+            AddInvoiceIncludes();
+        }
+
+
+        /// <summary>
+        /// Gom tất cả các include của Invoice vào 1 hàm riêng
+        /// </summary>
+        private void AddInvoiceIncludes()
+        {
+            ApplyInclude(i => i.Include(x => x.Table)
+                                .Include(x => x.Details)
+                                     .ThenInclude(x => x.OrderItem)
+                                          .ThenInclude(oi => oi.Order)
+
+                                .Include(x => x.Details)
+                                        .ThenInclude(d => d.OrderItem)
+                                            .ThenInclude(oi => oi.Product)
+                                                .ThenInclude(p => p.AvailableToppings)
+                           .Include(x => x.Details)
+                                        .ThenInclude(d => d.OrderItem)
+                                            .ThenInclude(oi => oi.Product)
+                                                .ThenInclude(p => p.Sizes));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
