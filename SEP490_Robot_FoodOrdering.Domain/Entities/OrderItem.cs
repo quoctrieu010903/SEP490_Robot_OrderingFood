48e3fc2e8085d132dbd6aefbde65729f65ac6916
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-09-16 13:03:42 +0700

    feature/update_price_order: when orderitem has status cancelled then update the price

--- SEP490_Robot_FoodOrdering.Domain/Entities/OrderItem.cs
+++ SEP490_Robot_FoodOrdering.Domain/Entities/OrderItem.cs
@@ -4,22 +4,22 @@ using SEP490_Robot_FoodOrdering.Domain.Enums;
 
 namespace SEP490_Robot_FoodOrdering.Domain.Entities
 {
-    public class OrderItem : BaseEntity
-    {
-        public Guid OrderId { get; set; }
-        public virtual Order Order { get; set; }
+        public class OrderItem : BaseEntity
+        {
+            public Guid OrderId { get; set; }
+            public virtual Order Order { get; set; }
 
-        public Guid ProductId { get; set; }
-        public virtual Product Product { get; set; }
+            public Guid ProductId { get; set; }
+            public virtual Product Product { get; set; }
 
-        public Guid ProductSizeId { get; set; }
-        public virtual ProductSize ProductSize { get; set; }
+            public Guid ProductSizeId { get; set; }
+            public virtual ProductSize ProductSize { get; set; }
 
-        public OrderItemStatus Status { get; set; } // Pending, Preparing, Ready, Served, Completed, Cancelled , Returned
-        public string? Note { get; set; }
-        public string? RemarkNote { get; set; } 
+            public OrderItemStatus Status { get; set; } // Pending, Preparing, Ready, Served, Completed, Cancelled , Returned
+            public string? Note { get; set; }
+            public string? RemarkNote { get; set; } 
 
-        public virtual ICollection<OrderItemTopping> OrderItemTopping { get; set; }
+            public virtual ICollection<OrderItemTopping> OrderItemTopping { get; set; }
 
         
         
