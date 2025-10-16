19b7f193094d3d41d8dea5afca65783173ae8734
thdat-vu <vuthanhdat181@gmail.com>
2025-10-16 21:24:35 +0700

    feat: add migration PayOSOrderCode column

---
+++ SEP490_Robot_FoodOrdering.Infrastructure/Migrations/20251016134336_AddCollumnPayOSOrderCodeForPayment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SEP490_Robot_FoodOrdering.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCollumnPayOSOrderCodeForPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PayOSOrderCode",
+                table: "Payments",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PayOSOrderCode",
+                table: "Payments");
+        }
+    }
+}
