691e879c8a280ba37044cc39a7599366a4e0abba
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-10-18 14:59:12 +0700

    complete complain service

---
+++ SEP490_Robot_FoodOrdering.API/Controllers/ComplainController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEP490_Robot_FoodOrdering.Application.DTO.Request.Complain;
+using SEP490_Robot_FoodOrdering.Application.DTO.Response.Complain;
+using SEP490_Robot_FoodOrdering.Application.Service.Interface;
+using SEP490_Robot_FoodOrdering.Core.Response;
+
+namespace SEP490_Robot_FoodOrdering.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ComplainController
+{
+    public readonly IComplainService FeedbackService;
+
+    public ComplainController(IComplainService feedbackService = null)
+    {
+        FeedbackService = feedbackService ?? throw new ArgumentNullException(nameof(feedbackService));
+    }
+
+
+    [HttpPost]
+    public Task<BaseResponseModel<ComplainCreate>> CreateFeedback([FromBody] ComplainRequests request)
+    {
+       var response =  FeedbackService.CreateComplainAsyns(request);
+        return response;
+    }
+
+
+    [HttpGet("{idTable}")]
+    public Task<BaseResponseModel<List<ComplainResponse>>> GetFeedbackTable(Guid idTable)
+        => FeedbackService.GetComplainByTable(idTable);
+
+    [HttpPut("{idTable}")]
+    public Task<BaseResponseModel<List<ComplainCreate>>> ConfirmFeedback(
+        Guid idTable,
+        [FromQuery] List<Guid> idFeedback,
+        [FromQuery] string content,
+        [FromQuery] bool isPeeding)
+        => FeedbackService.ComfirmComplain(idTable, idFeedback, isPeeding, content);
+
+
+    [HttpGet]
+    public Task<BaseResponseModel<Dictionary<string, ComplainPeedingInfo>>> GetAllFeedbackIsPeeding()
+        => FeedbackService.GetAllComplainIsPending();
+}
---
+++ SEP490_Robot_FoodOrdering.Application/DTO/Response/Complain/ComplainPendingInfo.cs
@@ -0,0 +1,5 @@
+﻿namespace SEP490_Robot_FoodOrdering.Application.DTO.Response.Complain;
+
+public record ComplainPeedingInfo(string TableName , int Counter  , int DeliveredCount, int ServeredCount,
+    int PaidCount,
+    int TotalItems);
