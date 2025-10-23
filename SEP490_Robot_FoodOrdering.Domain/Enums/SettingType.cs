7277ffb90e8a37e9604705295ca29b4f86098c7b
thdat-vu <vuthanhdat181@gmail.com>
2025-10-23 16:54:39 +0700

    chore: add number to setting type enum

--- SEP490_Robot_FoodOrdering.Domain/Enums/SettingType.cs
+++ SEP490_Robot_FoodOrdering.Domain/Enums/SettingType.cs
@@ -2,10 +2,10 @@ namespace SEP490_Robot_FoodOrdering.Domain.Enums;
 
 public enum SettingType
 {
-    String,
-    Int,
-    Bool,
-    DateTime,
-    Date,
-    Json,
+    String = 1,
+    Int= 2,
+    Bool= 3,
+    DateTime= 4,
+    Date= 5,
+    Json= 6
 }
