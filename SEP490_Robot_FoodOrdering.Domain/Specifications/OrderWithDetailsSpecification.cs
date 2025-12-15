2fb9d521a011ef9a8f687a736d60615f520f2949
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-12-15 22:05:33 +0700

    fix get order has session is active

--- SEP490_Robot_FoodOrdering.Domain/Specifications/OrderWithDetailsSpecification.cs
+++ SEP490_Robot_FoodOrdering.Domain/Specifications/OrderWithDetailsSpecification.cs
@@ -7,7 +7,7 @@ public class OrderWithDetailsSpecification : BaseSpecification<Order>
 {
     public OrderWithDetailsSpecification(string token , string idTable)
         : base(order => order.Table.DeviceId == token && order.TableId.ToString() == idTable &&
-    order.CreatedTime.Date == DateTime.UtcNow.Date)
+    order.CreatedTime.Date == DateTime.UtcNow.Date && order.TableSession.Status == Enums.TableSessionStatus.Active)
     {
         AddIncludes();
     }
@@ -15,13 +15,14 @@ public class OrderWithDetailsSpecification : BaseSpecification<Order>
     {
         ApplyInclude(q => q
             .Include(o => o.OrderItems)
-            .ThenInclude(oi => oi.Product)
+                    .ThenInclude(oi => oi.Product)
             .Include(o => o.OrderItems)
-            .ThenInclude(oi => oi.ProductSize) 
+                    .ThenInclude(oi => oi.ProductSize) 
             .Include(o => o.OrderItems)
-            .ThenInclude(oi => oi.OrderItemTopping)
+                     .ThenInclude(oi => oi.OrderItemTopping)
             .ThenInclude(oi => oi.Topping)
             .Include(o => o.Table)
+                     .ThenInclude(o=> o.Sessions)
             .Include(o => o.Payments));
     } 
 }
