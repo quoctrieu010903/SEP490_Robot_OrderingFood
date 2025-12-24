97c1a7684a5d552e4e05ea5e5419fb045347dd5e
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-12-24 11:24:37 +0700

    fix dataa

--- SEP490_Robot_FoodOrdering.Application/Service/Interface/IComplainService.cs
+++ SEP490_Robot_FoodOrdering.Application/Service/Interface/IComplainService.cs
@@ -7,7 +7,7 @@ namespace SEP490_Robot_FoodOrdering.Application.Service.Interface;
 public interface IComplainService
 {
     Task<BaseResponseModel<ComplainCreate>> CreateComplainAsyns(ComplainRequests request);
-    Task<BaseResponseModel<List<ComplainResponse>>> GetComplainByTable1(Guid idTable, bool forCustomer = false);
+    Task<BaseResponseModel<List<ComplainResponse>>> GetComplainByTable(Guid idTable, bool forCustomer = false);
     Task<BaseResponseModel<Dictionary<string, ComplainPeedingInfo>>> GetAllComplainIsPending();
     Task<BaseResponseModel<List<ComplainCreate>>> ComfirmComplain(Guid idTable, List<Guid>? IDFeedback, bool isPeeding,string content);
 }
